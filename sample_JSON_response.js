33b85915aaf7f0bb4fff89aa3c2c30ab610420e5
Daniel Lai <dlai813@gmail.com>
2019-03-27 00:28:41 -0400

    [Init] project structure set up

---
+++ sample_JSON_response.js
@@ -0,0 +1,59 @@
+// Example Vehicle Product Endpoint
+const endpoint = `${ROOT_URL}vehicles/:vifnum/products/:productId/:productTypeId`;
+
+const sampleDate = {
+	status: "success",
+	vehicle: {
+		vifnum: 10155,
+		orgnum: 0,
+		sendnum: 10155,
+		year: 2015,
+		make: "Chevrolet",
+		model: "Cruze",
+		trim: "Eco Automatic",
+		doors: 4,
+		body: "sedan",
+		cab: "",
+		wheels: "FWD",
+		vin: "1G1PH5SB7F7131633",
+		delivered: "2014-12-30",
+		color_title: "Tungsten Metallic",
+		color_simpletitle: "Gray",
+		year_str: "",
+		vehicle_str: "2015 Chevrolet Cruze Eco Automatic - Brown FWD sedan",
+		spec_color_angles: 3,
+		spec_video_spins: 1,
+		spec_video_clips: 1,
+		spec_stills_white: 1,
+		spec_vr_avail: 0,
+		spec_vr2_avail: 0,
+		vr_qa_hold: "0",
+		fulltext_search:
+			"2015 chevrolet cruze eco automatic  sedan fwd tungsten metallic brown",
+		keywords: "",
+		max_trim: "0",
+		fuel_type: "",
+		fuel_alt: 0,
+		send_year: 2015,
+		id: 885029,
+		last_update: "2019-02-05T07:42:44.000Z",
+		dataone_id: null
+	},
+	product: {
+		product_id: 2,
+		product_type_id: 41,
+		name: "color_0640_032_png"
+	},
+	urls: [
+		"https://dbhdyzvm8lm25.cloudfront.net/color_0640_032_png/MY2015/10155/10155_cc0640_032_G1E.png",
+		"https://dbhdyzvm8lm25.cloudfront.net/color_0640_032_png/MY2015/10155/10155_cc0640_032_G1F.png",
+		"https://dbhdyzvm8lm25.cloudfront.net/color_0640_032_png/MY2015/10155/10155_cc0640_032_G7C.png",
+		"https://dbhdyzvm8lm25.cloudfront.net/color_0640_032_png/MY2015/10155/10155_cc0640_032_G7J.png",
+		"https://dbhdyzvm8lm25.cloudfront.net/color_0640_032_png/MY2015/10155/10155_cc0640_032_GAN.png",
+		"https://dbhdyzvm8lm25.cloudfront.net/color_0640_032_png/MY2015/10155/10155_cc0640_032_GAR.png",
+		"https://dbhdyzvm8lm25.cloudfront.net/color_0640_032_png/MY2015/10155/10155_cc0640_032_GAZ.png",
+		"https://dbhdyzvm8lm25.cloudfront.net/color_0640_032_png/MY2015/10155/10155_cc0640_032_GWT.png",
+		"https://dbhdyzvm8lm25.cloudfront.net/color_0640_032_png/MY2015/10155/10155_cc0640_032_GXG.png",
+		"https://dbhdyzvm8lm25.cloudfront.net/color_0640_032_png/MY2015/10155/10155_cc0640_032_GXH.png"
+	]
+};
