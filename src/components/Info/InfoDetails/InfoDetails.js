a15a13b31517560571076638f8ff1ef1e6723e7e
Daniel Lai <dlai813@gmail.com>
2019-03-28 16:00:27 -0400

    [In Prog] fine tune info details spacing

--- src/components/Info/InfoDetails/InfoDetails.js
+++ src/components/Info/InfoDetails/InfoDetails.js
@@ -6,63 +6,73 @@ class InfoDetails extends Component {
 	render() {
 		return (
 			<div styleName="infoDetails">
-				<section styleName="borderBottom">
+				<section styleName="borderBottom marginBottom-M">
 					<div styleName="row">
-						<h4 styleName="font-L">2015 Chevrolet</h4>
-						<h3 styleName="font-XL">Cruze LS</h3>
+						<h4 styleName="font-M-Plus">2015 Chevrolet</h4>
+						<h3 styleName="font-XL marginBottom-M">Cruze LS</h3>
 						<div styleName="col-1-of-2">
-							<p styleName="font-L primary-light">42,052 Mi.</p>
+							<p styleName="font-M-Plus marginBottom-M primary-light">
+								42,052 Mi.
+							</p>
 						</div>
 						<div styleName="col-1-of-2">
-							<a href="#" styleName="font-S grey-dark-3 uppercase">
+							<a href="#" styleName="font-M grey-dark-3 uppercase">
 								Vehicle features &rsaquo;
 							</a>
 						</div>
 					</div>
 				</section>
 
-				<section styleName="borderBottom">
+				<section styleName="borderBottom marginBottom-XL">
 					<div styleName="row">
 						<div styleName="col-1-of-2">
-							<h3 styleName="font-L black">Exterior Color:</h3>
-							<p styleName="font-M grey-dark">Glacier Silver Metalic</p>
+							<h3 styleName="font-M-Plus marginBottom-S black">
+								Exterior Color:
+							</h3>
+							<p styleName="font-M marginBottom-L grey-dark">
+								Glacier Silver Metalic
+							</p>
 						</div>
 						<div styleName="col-1-of-2">
-							<h3 styleName="font-L black">Interior Colors:</h3>
-							<p styleName="font-M grey-dark">Black Onyx</p>
+							<h3 styleName="font-M-Plus marginBottom-S black">
+								Interior Colors:
+							</h3>
+							<p styleName="font-M marginBottom-L grey-dark">Black Onyx</p>
 						</div>
 					</div>
 				</section>
 
 				{/* Payment Options */}
-				<section styleName="borderBottom">
+				<section styleName="borderBottom marginBottom-M">
 					<div styleName="row">
 						<div styleName="col-1-of-2">
-							<h3 styleName="font-L black">Monthly Pymt.</h3>
-							<p styleName="font-XXL primary-dark">$270</p>
+							<h3 styleName="font-L marginBottom-M black">Monthly Pymt.</h3>
+							<p styleName="font-XXL marginBottom-L primary-dark">$270</p>
 						</div>
 						<div styleName="col-1-of-2">
-							<h3 styleName="font-L black">Start Pymt.</h3>
-							<p styleName="font-XXL grey-dark-3">$500</p>
+							<h3 styleName="font-L marginBottom-M black">Start Pymt.</h3>
+							<p styleName="font-XXL marginBottom-L grey-dark-3">$500</p>
 						</div>
 					</div>
 				</section>
 
 				{/* Alternative */}
-				<section styleName="borderBottom">
+				<section styleName="borderBottom marginBottom-M">
 					<div styleName="row">
 						<div styleName="col-1-of-2">
-							<h3 styleName="font-M grey-dark">Monthly Pymt.</h3>
-							<p styleName="font-XL grey-dark-2">$390</p>
+							<h3 styleName="font-M marginBottom-S grey-dark">Monthly Pymt.</h3>
+							<p styleName="font-XL marginBottom-M grey-dark-2">$390</p>
 						</div>
 						<div styleName="col-1-of-2">
-							<h3 styleName="font-M grey-dark">Down Pymt.</h3>
-							<p styleName="font-XL grey-dark-2">$800</p>
+							<h3 styleName="font-M marginBottom-S grey-dark">Down Pymt.</h3>
+							<p styleName="font-XL marginBottom-M grey-dark-2">$800</p>
 						</div>
 					</div>
-					<a href="#" styleName="font-S black uppercase">
-						Learn more &rsaquo;
-					</a>
+					<div styleName="marginBottom-L">
+						<a href="#" styleName="font-M black uppercase">
+							Learn more &rsaquo;
+						</a>
+					</div>
 				</section>
 			</div>
 		);
